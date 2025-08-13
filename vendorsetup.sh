d54f0d1ead17a2543fffac4867d3bea521e92440
koaaN <salomannen1234@gmail.com>
2025-08-13 18:33:55 +0200

    Add dynamic partition tools instead of providing static

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -79,5 +79,6 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_MAINTAINER="Jonas Salo & Daniel Springer"
 	export OF_SCREEN_H=2376
  	export TARGET_DEVICE_ALT="OP5D55L1, OP5D0DL1"
+  	export OF_ENABLE_ALL_PARTITION_TOOLS=1
 fi
 #
